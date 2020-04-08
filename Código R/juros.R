e60053cdb89bbc64583346a6c2e25e8c4f189074
caio-azevedo <caio.azevedo@live.com>
2020-04-07 21:26:39 -0300

    Figuras

--- Código R/juros.R
+++ Código R/juros.R
@@ -13,52 +13,50 @@ library(lattice)
 
 # Exportando os dados disponíveis no GitHub
 
-site <- "https://raw.githubusercontent.com/caio-azevedo/Series-Temporais/master/data/Taxa%20de%20juros.csv"
+site <- "https://raw.githubusercontent.com/caio-azevedo/Series-Temporais/master/data/passageiros_embarcados.csv"
 
 dados<- read.table(site, header=T, sep=";", dec = ",", 
-                   col.names = c("data", "juros", ""))
+                   col.names = c("data", "pas", ""))
 
 
 # Configurando os dados para o formato de séries temporais
 attach(dados)
 
-juros74 <-ts(juros, start=c(1974,1), frequency = 12)
+pas69 <-ts(pas, start=c(1969), frequency = 1)
+
+plot(pas69)
+
+dev.copy(pdf,"pas69.pdf")
+dev.off()
 
 
-# Fazendo um recorte de tempo nos dados a partir de 2010
 
-juros2010<- window(juros74, start=2010)
+# Aplicando a suavização exponencial de Holt
 
-# Plotando o gráfico da série
-plot(juros2010)
+pas_holt<-holt(pas69, level = 95, h=10)
 
 
-# Aplicando a suavização exponencial simples
-
-juros_ses<-ses(juros2010, level = 95, h=10)
+plot(pas_holt)
 
 
-plot(juros_ses)
+# Gerar uma nova série através da Suavização Exponencial de Holt (SEH)
+pas_prev<- pas_holt[["model"]][["fitted"]]
+pas_prev<-data.frame(pas_prev)
 
 
-# Gerar uma nova série através da Suavização Exponencial Simples (SES)
-juros_prev<- juros_ses[["model"]][["fitted"]]
-juros_prev<-data.frame(juros_prev)
+#Retirando os valores previstos da SEH para fora da amostra
 
-
-#Retirando os valores previstos da SES para fora da amostra
-
-x<-data.frame(juros_ses, row.names = c(1:10))
+x<-data.frame(pas_holt, row.names = c(1:10))
 x<-x %>% 
   select("Point.Forecast") %>% 
-  rename("juros_prev"="Point.Forecast")
+  rename("pas_prev"="Point.Forecast")
 
-juros_prev<-bind_rows(juros_prev, x)
+pas_prev<-bind_rows(pas_prev, x)
 
 
 
 # Juntar as duas séries - original e suavizada - em uma matriz para formar um gráfico único
-juros2010<-data.frame(juros2010)
+pas69<-data.frame(pas69)
 
 # é necessário criar um vetor nulo pra completar a série original
 # a repetição é de acordo com o número de períodos a ser previsto
@@ -66,26 +64,70 @@ nulo<-c(rep(NA,10))
 nulo<-data.frame(nulo)
 
 nulo<-nulo %>% 
-  rename("juros2010"="nulo")
+  rename("pas69"="nulo")
 
-juros2010<-bind_rows(juros2010, nulo)
+pas69<-bind_rows(pas69, nulo)
 
-graf<-cbind(juros2010, juros_prev)
-colnames(graf)<-c("Série Original", "SES")
+graf<-cbind(pas69, pas_prev)
+colnames(graf)<-c("Série Original", "Holt")
 
 
 # Retornando para o formato temporal
 
-graf_ts<-ts(graf, start=c(2010,1), frequency = 12)
+graf_ts<-ts(graf, start=c(1969), frequency = 1)
 
 
 # Plotar um único gráfico
 xyplot(graf_ts, superpose = TRUE, lwd = 2) 
 
-dev.copy(pdf,"juros.pdf")
+dev.copy(pdf,"pas.pdf")
 dev.off()
 
 
+#Encontrar os parâmetros estimados
+
+parametros<-data.frame(round(pas_holt[["model"]][["par"]], digits = 4))
+parametros<-data.frame(parametros[1:2,])
+colnames(parametros)<-c("Parâmetros estimados")
+rownames(parametros)<-c("alpha", "beta")
+
+# Estados Iniciais
+
+estado_inic<-data.frame(round(pas_holt[["model"]][["initstate"]], digits = 2))
+colnames(estado_inic)<-c("Estados Iniciais")
+
+#Critérios de informação
+
+loglik <- pas_holt[["model"]][["loglik"]]
+aic <- pas_holt[["model"]][["aic"]]
+bic <- pas_holt [["model"]][["bic"]]
+aicc <- pas_holt[["model"]][["aicc"]]
+mse <- pas_holt[["model"]][["mse"]]
+amse <- pas_holt[["model"]][["amse"]]
+
+inf_crit<-data.frame(c(loglik, aic, bic, aicc, mse, amse))
+colnames(inf_crit)<-c("Critérios de Informação")
+rownames(inf_crit)<-c("LogLik", "AIC", "BIC", "AICC", "MSE", "AMSE")
+
+print(xtable(estado_inic, caption = "Estados Iniciais utilizados na Previsão da SEH", 
+             label = "tab2.2", digits = 0),
+      caption.placement = "top",
+      include.rownames = TRUE,
+      format.args = list(big.mark = ".", decimal.mark = ","))
+
+print(xtable(inf_crit, caption = "Critérios de Informação da SEH", 
+             label = "tab2.3"),
+      caption.placement = "top",
+      include.rownames = TRUE,
+      format.args = list(big.mark = ".", decimal.mark = ","))
+
+holt<-data.frame(pas_holt)
+print(xtable(holt, caption = "Previsão para a quantidade de passageiros embarcados
+             através do Modelo de Holt", 
+             label = "tab2.4"),
+      caption.placement = "top",
+      include.rownames = TRUE,
+      format.args = list(big.mark = ".", decimal.mark = ","))
 
 
 
